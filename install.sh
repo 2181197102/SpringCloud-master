93182e6a46c0ca72aa9aa15585d614bc3668b10c
Zeno <2181197102@qq.com>
2024-01-29 19:52:20 +0800

    clean docker

--- install.sh
+++ install.sh
@@ -21,12 +21,12 @@ esac
 
 echo '==================1.3清理当前脚本启动的容器和产生的镜像(可选的)=============='
 #清理当前脚本启动的容器和产生的镜像(可选的)
-#docker stop sc-rabbitmq sc-redis sc-mysql
-#docker rm sc-rabbitmq sc-redis sc-mysql
+docker stop sc-rabbitmq sc-redis sc-mysql
+docker rm sc-rabbitmq sc-redis sc-mysql
 #docker image rm rabbitmq:alpine redis:alpine mysql:9.6-alpine
 
-#docker stop sc-monitor-admin sc-authorization-server sc-authentication-server sc-organization sc-gateway-admin sc-gateway-web
-#docker rm sc-monitor-admin sc-authorization-server sc-authentication-server sc-organization sc-gateway-admin sc-gateway-web
+docker stop sc-monitor-admin sc-authorization-server sc-authentication-server sc-organization sc-gateway-admin sc-gateway-web
+docker rm sc-monitor-admin sc-authorization-server sc-authentication-server sc-organization sc-gateway-admin sc-gateway-web
 #docker image rm cike/admin cike/authorization-server:latest cike/authentication-server:latest cike/organization:latest cike/gateway-admin:latest cike/gateway-web:latest
 mvn -pl ./common/core,./facade,./auth/authentication-client install -Dmaven.test.skip=true
 
